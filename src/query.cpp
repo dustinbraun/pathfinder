65d760ea314b1d818e4844f3c06ddeed363cbac0
Dustin Braun <dustin.braun@pm.me>
2023-04-27 17:42:25 +0200

    Fixed a bug where an 0-valued node-position was used for g-value computation whenever a new node was created in the hash-map during search. The correct position (edge center point) will be used now.

--- src/query.cpp
+++ src/query.cpp
@@ -66,22 +66,19 @@ Query::expand_node(
         if (next_face_id != FACE_ID_NONE) {
             Node * next_node = m_node_arena.get_node(node.m_next_face, next_face_id);
             if (next_node != nullptr && !next_node->m_state.m_is_closed) {
-                float g = node.m_state.m_g + compute_distance(node.m_state.m_pos, next_node->m_state.m_pos);
-                auto compute_f = [&] {
-                    return g + compute_distance(next_node->m_state.m_pos, end_pos);
-                };
                 if (next_node->m_state.m_is_open) {
+                    float g = node.m_state.m_g + compute_distance(node.m_state.m_pos, next_node->m_state.m_pos);
                     if (g < next_node->m_state.m_g) {
                         next_node->m_state.m_g = g;
-                        next_node->m_state.m_f = compute_f();
+                        next_node->m_state.m_f = g + compute_distance(next_node->m_state.m_pos, end_pos);
                         next_node->m_state.m_parent_node = &node;
                         m_node_queue.modify_node((*next_node));
                     }
                 }
                 else {
                     next_node->m_state.m_pos = mesh.get_edge_center_point(mesh.get_face_by_id(node.m_next_face), edge_index);
-                    next_node->m_state.m_g = g;
-                    next_node->m_state.m_f = compute_f();
+                    next_node->m_state.m_g = node.m_state.m_g + compute_distance(node.m_state.m_pos, next_node->m_state.m_pos);;
+                    next_node->m_state.m_f = next_node->m_state.m_g + compute_distance(next_node->m_state.m_pos, end_pos);
                     next_node->m_state.m_parent_node = &node;
                     next_node->m_state.m_is_open = true;
                     m_node_queue.insert_node(next_node);
